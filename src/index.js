35e35c86c94fabfb479c61aa1459abd4122e0d16
Peter_Lloyd <peterl@fisheyehq.com>
2018-08-09 22:33:59 +0100

    NS-01 add App and index files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import Store from './redux/Store';
+import App from "./App";
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
