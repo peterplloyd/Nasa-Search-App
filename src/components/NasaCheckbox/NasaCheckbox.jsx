6c01b413c7bc1f83389d7f8ea711601f9317dbee
Peter_Lloyd <peterl@fisheyehq.com>
2018-08-11 00:03:44 +0100

    NS-01 update checkbox so it has it’s own custom styling and is formatted correctly

--- src/components/NasaCheckbox/NasaCheckbox.jsx
+++ src/components/NasaCheckbox/NasaCheckbox.jsx
@@ -1,44 +1,47 @@
-import React, { Component } from 'react'
-import styles from './NasaCheckbox.scss'
+import React, { Component } from "react";
+import styles from "./NasaCheckbox.scss";
 
 class NasaCheckbox extends Component {
   state = {
-    isChecked: true,
-  }
+    isChecked: true
+  };
 
   toggleCheckboxChange = () => {
     this.setState(({ isChecked }) => ({
-      isChecked: !isChecked,
-    }))
-  }
+      isChecked: !isChecked
+    }));
+  };
 
   render() {
-    const { label } = this.props
-    const { mediaType } = this.props
-    const { isChecked } = this.state
+    const { label } = this.props;
+    const { mediaType } = this.props;
+    const { isChecked } = this.state;
     const {
       searchHeader__container__checkbox__container,
       searchHeader__container__checkbox__container__input,
-      searchHeader__container__checkbox__container__label,
-    } = styles
+      searchHeader__container__checkbox__container__label
+    } = styles;
 
     return (
       <div className={searchHeader__container__checkbox__container}>
-        <label className={searchHeader__container__checkbox__container__label}>
-          <input
-            className={searchHeader__container__checkbox__container__input}
-            id={mediaType}
-            type="checkbox"
-            value={label}
-            checked={isChecked}
-            aria-checked={isChecked}
-            onChange={this.toggleCheckboxChange}
-          />
+        <input
+          className={searchHeader__container__checkbox__container__input}
+          id={mediaType}
+          type="checkbox"
+          value={label}
+          checked={isChecked}
+          aria-checked={isChecked}
+          onChange={this.toggleCheckboxChange}
+        />
+        <label
+          htmlFor={mediaType}
+          className={searchHeader__container__checkbox__container__label}
+        >
           {label}
         </label>
       </div>
-    )
+    );
   }
 }
 
-export default NasaCheckbox
+export default NasaCheckbox;
