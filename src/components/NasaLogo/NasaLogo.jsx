0e00eb246862b2d0bb248dcbb2bdb3fca3615649
Peter Lloyd <peter.lloyd@missguided.com>
2018-08-10 11:43:55 +0100

    NS-01 Use BEM for classes

--- src/components/NasaLogo/NasaLogo.jsx
+++ src/components/NasaLogo/NasaLogo.jsx
@@ -1,41 +1,47 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import IconLogo from '../../images/svg/logo.svg'
-import styles from './NasaLogo.scss'
+import PropTypes from "prop-types";
+import React from "react";
+import IconLogo from "../../images/svg/logo.svg";
+import styles from "./NasaLogo.scss";
 
 const NasaLogo = ({ href, title, alt }) => {
-  const { header, logoContainer, logo } = styles
-  let headerElm
+  const {
+    logoHeader,
+    logoHeader__link,
+    logoHeader__container,
+    logoHeader__container__logo
+  } = styles;
+  
+  let headerElm;
 
   if (href) {
     headerElm = (
-      <a href={href}>
-        <div className={logoContainer}>
+      <a className={logoHeader__link} href={href}>
+        <div className={logoHeader__container}>
           <title>{title}</title>
-          <IconLogo className={logo} alt={alt}/>
+          <IconLogo className={logoHeader__container__logo} alt={alt} />
         </div>
       </a>
-    )
+    );
   } else {
     headerElm = (
-      <div className={logoContainer}>
+      <div className={logoHeader__container}>
         <title>{title}</title>
-        <IconLogo className={logo} alt={alt}/>
+        <IconLogo className={logoHeader__container__logo} alt={alt} />
       </div>
-    )
+    );
   }
 
   return (
-    <header title={title} href={href} className={header}>
+    <header title={title} href={href} className={logoHeader}>
       {headerElm}
     </header>
-  )
-}
+  );
+};
 
 NasaLogo.propTypes = {
   href: PropTypes.string,
   title: PropTypes.string,
   alt: PropTypes.string
-}
+};
 
-export default NasaLogo
+export default NasaLogo;
